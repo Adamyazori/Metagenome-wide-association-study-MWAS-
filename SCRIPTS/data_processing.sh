35fd5d53ef736c16a6b1de00acd38938180486e8
Seidu Adams <76058754+Adamyazori@users.noreply.github.com>
2025-02-05 10:37:19 -0500

    Update data_processing.sh

--- SCRIPTS/data_processing.sh
+++ SCRIPTS/data_processing.sh
@@ -1,72 +1,59 @@
 #!/bin/bash
 
-# Kill script if any commands fail
-set -e
-echo "Job Start at `date`"
 
-mkdir 02_Assembly
+mkdir 00_RawData 01_CleanData
 
-##############Part2 Assembly: CleanData to Contigs###########################
-megahit=~/bin/megahit
+##############Part1 QC: Rawdata to CleanData###########################
+fastp=~/bin/fastp
+bwa=~/bin/bwa
+samtools=~/bin/samtools
+bedtools=~/bin/bedtools
+Ref=~/HostReads/BosT_Ref/bosTau9.fa
+Ref1=~/HostReads/HuManRef/homoSapien.all.fa
+RawData=./00_RawData
 CleanData=./01_CleanData
-Assembly=./02_Assembly
-bowtie2build=~/bin/bowtie2-build
-bowtie2=~/bin/bowtie2
 
-mkdir ./02_Assembly/Contigs ./02_Assembly/unmapped
-Contigs=./02_Assembly/Contigs
-unmapped=./02_Assembly/unmapped
+#Before performing the metagenomic pipeline, copy the raw sequencing data into the directory '00_Rawdata'.
+cd ${CleanData}
+##QC Step1: filter low quality sequence by fastp 
+${fastp} -i ${RawData}/${SampleID}_1.raw.fq.gz \
+-o ${SampleID}.clean_R1.fastq.gz \
+-I ${RawData}/${SampleID}_2.raw.fq.gz \
+-O ${SampleID}.clean_R2.fastq.gz -h ${SampleID}.filt.html \
+-j ${SampleID}.filt.json \
+--cut_by_quality3 \
+-W 4 -M 20 -n 5 -c -l 50 -w 3
 
-#single sample Assembly
-${megahit} -1 ${CleanData}/${SampleID}_final_R1.fastq.gz \
--2 ${CleanData}/${SampleID}_final_R2.fastq.gz \
---min-count 2 \
---k-min 27 \
---k-max 127 \
---k-step 10 \
---num-cpu-threads 20 \
---min-contig-len 1000 \
--o ${Assembly}/${SampleID}_megahit
 
-cp ${SampleID}_megahit/final.contigs.fa ${Contigs}/${SampleID}.fa
+##QC Step2: filt host sequence
+#index of reference genome
+${bwa} index -a bwtsw -p ~/bosTau9.fa.1 ${Ref}
+${bwa} index -a bwtsw -p ~/homoSapien.all.fa.1 ${Ref1}
 
-###acquire the unassembled reads
-cd ${Contigs}
-
-#build index
-${bowtie2build} ${SampleID}.fa ${SampleID}
-
-#aligning sequencing reads to assembled contigs
-${bowtie2} -p 8 -x ${SampleID} -1 ${CleanData}/${SampleID}_final_R1.fastq.gz -2 ${CleanData}/${SampleID}_final_R2.fastq.gz -S ${SampleID}.sam
+#mapping to reference
+${bwa} mem -t 8 -T 30 \
+${Ref} \
+${SampleID}.clean_R1.fastq.gz ${SampleID}.clean_R2.fastq.gz \
+-R '@RG\tID:${SampleID}_join\tPL:illumina\tSM:${SampleID}_join' | samtools view -S -b 
 
 #extract unmapped sequence
-${samtools} view -b -f 12 -F 256 ${SampleID}.sam >${SampleID}_unmapped.bam
+${samtools} view -b -f 12 -F 256 ${SampleID}.bam >${SampleID}_filter_host.bam
 
 #sorted the bam file
-${samtools} sort -n ${SampleID}_unmapped.bam -o ${SampleID}_unmapped_sorted.bam 
+${samtools} sort -n ${SampleID}_filter_host.bam -o ${SampleID}_filter_host_sorted.bam 
  
 #transform bam to fastq
-${bedtools} bamtofastq -i ${SampleID}_unmapped_sorted.bam -fq ${SampleID}_unmapped_r1.fastq -fq2 ${SampleID}_unmapped_r2.fastq
+${bedtools} bamtofastq -i ${SampleID}_filter_host_sorted.bam -fq ${SampleID}_filter_host_r1.fastq -fq2 ${SampleID}_filter_host_r2.fastq
+
+#Generate Reports
+${fastp} -A -G -Q -L \
+-i ${SampleID}_filter_host_r1.fastq \
+-I ${SampleID}_filter_host_r2.fastq \ 
+-o ${SampleID}_final_R1.fastq.gz \ 
+-O ${SampleID}_final_R2.fastq.gz \ 
+-h ${SampleID}_final.html \ 
+-j ${SampleID}_final.json 
 
 #return to initial directory
-cd ../../
-#merge all unmapped reads
-cat ${Contigs}/*unmapped_r1.fastq >${unmapped}/unmapped_r1.fastq
-cat ${Contigs}/*unmapped_r2.fastq >${unmapped}/unmapped_r2.fastq
+cd ../
 
-#all unmapped reads were co-assembled
-${megahit} -1 ${unmapped}/unmapped_r1.fastq \
--2 ${unmapped}/unmapped_r2.fastq \
---min-count 2 \
---k-min 27 \
---k-max 127 \
---k-step 10 \
---num-cpu-threads 20 \
---min-contig-len 1000 \
--o ${unmapped}/unmapped_megahit
-
-#merge all contigs from single sample assembled and co-assembled from unmapped reads
-cat ${Contigs}/*.fa ${unmapped}/unmapped_megahit/final.contigs.fa > ${Assembly}/allSample500.final_contigs.fasta
-
-#get time end the job
-echo "Job finished at:" `date`
