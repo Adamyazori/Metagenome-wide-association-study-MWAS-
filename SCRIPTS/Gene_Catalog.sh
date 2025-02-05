a161843d8d3630e3ef00aa336ced7bee0f5368f3
Seidu Adams <76058754+Adamyazori@users.noreply.github.com>
2025-02-05 11:37:23 -0500

    Update Gene_Catalog.sh

--- SCRIPTS/Gene_Catalog.sh
+++ SCRIPTS/Gene_Catalog.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+##############Part3 Construction of the gene catalog###########################
+CleanData=./01_CleanData
+Assembly=./02_Assembly
+GeneCatalog=./03_Gene_Catalog
+Scripts=~/Scripts
+
+module purge
+module load prodigal/2.60
+
+prodigal -i final.contigs.fa -p meta -d final.contigs.genes.fna -a final.contigs.genes.faa
+
+
 
